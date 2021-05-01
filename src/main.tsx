ccd9de55291d99546472e5c3004dc289c34ccced
linuxing3 <linuxing3@qq.com>
2021-05-01 15:38:46 -0400

    update css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import dva from 'dva'
 import App from './App'
 import models from './models'
-import './index.css'
+import './sytles/index.css'
 
 // 生成dva app
 const app = dva();
